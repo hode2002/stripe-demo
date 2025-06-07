f024b0933cf529966b5be5bef2eefd7813c5d4ea
hode2002 <hovande.dev.2002@gmail.com>
2025-06-07 22:34:01 +0700

    Enhance AppModule by integrating OrderModule, PaymentModule, ProductModule, CartModule, and AuthModule; implement SupabaseAuthGuard as a global guard for improved authentication management.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StripeModule } from './stripe/stripe.module';
 import { ConfigModule } from '@nestjs/config';
+import { SupabaseModule } from 'src/supabse/supabase.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { SupabaseAuthGuard } from 'src/auth/guards/supabase-auth.guard';
 
 @Module({
   imports: [
@@ -8,6 +16,18 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     StripeModule,
+    SupabaseModule,
+    OrderModule,
+    PaymentModule,
+    ProductModule,
+    CartModule,
+    AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: SupabaseAuthGuard,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule { }
